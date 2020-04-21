b75385a40c236314e3dc84e1fda01c897f885d3c
RD345 <43142825+RD345@users.noreply.github.com>
2020-04-21 18:21:42 -0400

    Reorganized teacher pages, work on question & exam

--- model/create_exam.js
+++ model/create_exam.js
@@ -111,3 +111,102 @@ function createExam()
     var response = process("create_exam", Array("message_type", "exam_id", "exam_name"));
     alert(response.exam_id);
 }
+
+function getQuestions(src)
+{
+    var data = new FormData();
+    data.append('message_type', 'get_questions');
+    var xml_request = createXMLRequest(data);
+    alert("test");
+    xml_request.onload = function() 
+    {
+        if (xml_request.status == 200) // If the response is good (HTML code 200)
+        {
+            if (this.response)
+            {
+                debug(this.response); // Sends to debug hidden.
+                
+                question_list = JSON.parse(this.response); // array of all questions
+                var topics = [];
+                
+                function findTopic(find) {return topics == find};
+                // Get topics:
+                for(var i = 0; i < question_list.length; i++) 
+                    if(!topics.find(findTopic))
+                        topics.push(question_list[i].topic);
+                
+                var questions = src; // Gets the form.
+
+                // Create a question div:
+                var question = document.createElement("div");
+                question.className = "question-div";
+        
+                // Create the button
+                var button = document.createElement("button");
+                var add_question = ""; // TODO Delete
+
+                // Create the label:
+                var label = document.createElement("label");
+                label.innerHTML = "Select Topic";
+                question.appendChild(label);
+                question.appendChild(document.createElement("br"));
+
+                // Create the question selecter:
+                var select = document.createElement("select");
+                select.id = "topic";
+                for(var i = 0; i < topics.length; i++) 
+                {
+                    // Add the current topic as an option:
+                    var option = document.createElement("option");
+                    option.innerHTML = topics[i]
+                    select.appendChild(option);
+                    select.appendChild(document.createElement("br"));
+                }
+                question.appendChild(select); // Adds the select to the question.
+                question.appendChild(document.createElement("br"));
+
+                // Create the label:
+                var label = document.createElement("label");
+                label.innerHTML = "Select Question";
+                question.appendChild(label);
+
+                // Add questions to drop-down:
+                var question_select = document.createElement("select");
+                question_select.id = "question_num";
+                question_select.onchange = function() {changeQuestion()};
+                question.appendChild(document.createElement("br"));
+
+                // Add the questions as options:
+                for(var i = 0; i < question_list.length; i++) 
+                {
+                    var question_option = document.createElement("option");
+                    question_option.innerText = question_list[i].questionID;
+                    question_select.appendChild(question_option);
+                }
+                question.appendChild(question_select);
+                question.appendChild(document.createElement("br"));
+
+                // Create the label for the question description:
+                var label = document.createElement("label");
+                label.innerHTML = "Description:";
+                question.appendChild(label);
+                question.appendChild(document.createElement("br"));
+
+                // Create the description textarea:
+                var description = document.createElement("textarea");
+                description.id = "description";
+                description.readOnly = true;
+                description.innerText = question_list[0].description
+                question.appendChild(description);
+                question.appendChild(document.createElement("br"));
+
+                questions.appendChild(question);
+                // for(var i = 0; i < question_list.length; i++) 
+                //     var question = question_list[i];
+
+            }
+        } else 
+        alert("Server error!");
+    }
+    xml_request.send(data); // Sends the request for the questions.
+}
--- model/scripts.js
+++ model/scripts.js
@@ -138,3 +138,22 @@ function getFooter()
     footer.innerHTML = "<p class='p-1'>Created by Group 10: Ryan Doherty, Matt and Feiyang Wang</p>";
     document.getElementsByTagName("body").appendChild(footer);
 }
+
+function playSound(src)
+{
+    // this.sound = document.createElement("audio");
+    mySound = new sound("../sounds/bleat.wav");
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    } 
+}
+
